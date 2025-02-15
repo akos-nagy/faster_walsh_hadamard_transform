673746d32ccdfacad21610301cbb8e56e02e279d
Ákos Nagy <61956085+akos-nagy@users.noreply.github.com>
2025-02-14 19:02:31 -0500

    Update test_fwht.py

--- tests/test_fwht.py
+++ tests/test_fwht.py
@@ -1,14 +1,17 @@
 from faster_walsh_hadamard_transform import fwht
 import numpy as np
 
+import numpy as np
+from faster_walsh_hadamard_transform import fwht
 # Create an array of random integers with length 2^4 = 16
 data = np.random.randint(- 10, 10, 1 << 3)
+print("Original data\t= ", data)
 # Example array:
 # data = [ 3,  8,  9,  8, -8,  3,  9,  7]
 
-# Compute the Walsh-Hadamard Transform
+# Compute the Walsh–Hadamard Transform
 result = fwht(data)
 # Example output:
 # result = [ 39, -13, -27, -19,  17,   5,  15,   7]
 print("Original again\t= ", data)  # Should remain unchanged
-print("Transformed\t= ", result)  # WHT-transformed output
+print("Transformed\t= ", result)  # Transformed output
